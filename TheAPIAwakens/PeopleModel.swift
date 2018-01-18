0150ebdf81e8350d5e2a35dee2b00476a726998d
rrohweder <RogerRohweder@yahoo.com>
2018-01-18 15:33:10 -0500

    Initial Commit

---
+++ TheAPIAwakens/PeopleModel.swift
@@ -0,0 +1,13 @@
+//
+//  PeopleModel.swift
+//  TheAPIAwakens
+//
+//  Created by Roger Rohweder on 1/18/18.
+//  Copyright © 2018 Roger Rohweder. All rights reserved.
+//
+
+import Foundation
+
+// https://swapi.co/api/people/schema
+
+

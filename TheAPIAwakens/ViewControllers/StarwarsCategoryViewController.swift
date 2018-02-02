f1257144cd144d38d40feb0207c825b4b7382b5c
rrohweder <RogerRohweder@yahoo.com>
2018-02-01 21:16:44 -0500

    Fixed case on outlets in StarwarsCategoryViewControler.swift.  Added outlets for returnLinkLabel and categoryLabel. Added picker code.

--- TheAPIAwakens/ViewControllers/StarwarsCategoryViewController.swift
+++ TheAPIAwakens/ViewControllers/StarwarsCategoryViewController.swift
@@ -1,5 +1,5 @@
 //
-//  StarwarsCategoryController.swift
+//  StarwarsCategoryViewController.swift
 //  TheAPIAwakens
 //
 //  Created by Roger Rohweder on 1/24/18.
@@ -8,36 +8,60 @@
 
 import UIKit
 
-class StarwarsCategoryViewController: UIViewController {
-    var category: StarwarsCategory? = nil
+class StarwarsCategoryViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
-    @IBOutlet weak var Field1Label: UILabel!
-    @IBOutlet weak var Field1Value: UILabel!
-    @IBOutlet weak var Field2Label: UILabel!
-    @IBOutlet weak var Field2Value: UILabel!
-    @IBOutlet weak var CurrencyUSDLabel: UILabel!
-    @IBOutlet weak var CurrencyCreditsLabel: UILabel!
-    @IBOutlet weak var Field3Label: UILabel!
-    @IBOutlet weak var Field3Value: UILabel!
-    @IBOutlet weak var UnitsEnglishLabel: UILabel!
-    @IBOutlet weak var UnitsMetricLabel: UILabel!
-    @IBOutlet weak var Field4Label: UILabel!
-    @IBOutlet weak var Field4Value: UILabel!
+    var category: StarwarsCategory? = nil
+    var pickerData: [String] = [String]()
+
+    @IBOutlet weak var returnLinkLabel: UILabel!
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var field1Label: UILabel!
+    @IBOutlet weak var field1Value: UILabel!
+    @IBOutlet weak var field2Label: UILabel!
+    @IBOutlet weak var field2Value: UILabel!
+    @IBOutlet weak var currencyUSDLabel: UILabel!
+    @IBOutlet weak var currencyCreditsLabel: UILabel!
+    @IBOutlet weak var field3Label: UILabel!
+    @IBOutlet weak var field3Value: UILabel!
+    @IBOutlet weak var unitsEnglishLabel: UILabel!
+    @IBOutlet weak var unitsMetricLabel: UILabel!
+    @IBOutlet weak var field4Label: UILabel!
+    @IBOutlet weak var field4Value: UILabel!
+    @IBOutlet weak var field5Label: UILabel!
+    @IBOutlet weak var field5Value: UILabel!
+    
+    @IBOutlet weak var picker: UIPickerView!
     
     override func viewDidLoad() {
         
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        Field1Label.text = String(describing: category!)
+        field1Label.text = String(describing: category!)
+        pickerData = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6"]
+        // Connect data
+        self.picker.dataSource = self;
+        self.picker.delegate = self;
+        
+        
+        
         // OK, we have successfully set and passed the category.
         
         // next: load and enable the picker, and load the selected values for that category
         
     }
 
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
     
-    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
     
     
     
